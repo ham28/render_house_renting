7bdd0c05e04b1cd7d99142f696435855f6f276ec
Hasina-ades <hasindraibe.randriatsarafara@ades-solaire.org>
2025-05-02 21:22:46 +0300

    update fixtures

--- property_managment/management/commands/custom_dumpdata.py
+++ property_managment/management/commands/custom_dumpdata.py
@@ -1,8 +1,9 @@
 from django.core.management.base import BaseCommand
 from django.core import serializers
 
-from property_managment.models import Property, Owner
+from property_managment.models import Property, Owner, PropertyImages
 
+from user_managment.models import CustomUser
 
 
 class Command(BaseCommand):
@@ -17,4 +18,12 @@ class Command(BaseCommand):
 
         data = serializers.serialize('json', Owner.objects.all())
         with open('fixtures/Owner.json', 'w', encoding='utf-8') as f:
+            f.write(data)
+
+        data = serializers.serialize('json', CustomUser.objects.all())
+        with open('fixtures/CustomUser.json', 'w', encoding='utf-8') as f:
+            f.write(data)
+
+        data = serializers.serialize('json', PropertyImages.objects.all())
+        with open('fixtures/PropertyImages.json', 'w', encoding='utf-8') as f:
             f.write(data)
